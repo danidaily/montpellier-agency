ac53d8c061fbb65a3b075b73ae7e1f05d20e6706
danidaily <danylokazakovyur@gmail.com>
2021-02-12 14:19:45 +0200

    Montepellier Agency

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-/*js начнет обрабатываться только когда полностью загрузится html*/
-$(function () {
-    $('.slider_inner').slick();
+$(function(){
+
+  $('.slider__inner, .news__slider-inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false
+  });
+
+  $('select').styler();
+
+  $('.header__btn-menu').on('click', function(){
+    $('.menu ul').slideToggle();
+  });
+
 });
